f69a568b382b7f96c765b76c5b994cdc6210e141
Mohammad Azimifar PC <mmazimifar7@gmail.com>
2025-12-21 15:56:29 +0330

    Feat(infrastructure layer tests): Add unit tests for user service

--- src/Weblu.Application/Dtos/UserDtos/UserDto.cs
+++ src/Weblu.Application/Dtos/UserDtos/UserDto.cs
@@ -9,14 +9,14 @@ namespace Weblu.Application.Dtos.UserDtos
 {
     public class UserDto
     {
-        public required string Id { get; set; }
+        public string Id { get; set; } = default!;
         public required string PhoneNumber { get; set; }
         public required string UserName { get; set; }
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
         public required string FullName { get; set; }
         public List<string> Roles { get; set; } = new List<string>();
-        public required string CreatedAt { get; set; }
+        public string CreatedAt { get; set; } = default!;
         public string? UpdatedAt { get; set; }
         public List<ProfileDto>? Profiles { get; set; }
     }
