1fd773961e0fde6f80e3ac5e52e792764bb4404d
Mohammad Azimifar PC <mmazimifar7@gmail.com>
2025-12-27 21:45:03 +0330

    Refactore(seed methods): seed users and roles just via DbContext dependency

--- src/Weblu.Infrastructure/Extensions/DatabaseConfigurations.cs
+++ src/Weblu.Infrastructure/Extensions/DatabaseConfigurations.cs
@@ -31,11 +31,9 @@ namespace Weblu.Infrastructure.Extensions
             {
                 var services = scope.ServiceProvider;
                 var dbContext = services.GetRequiredService<ApplicationDbContext>();
-                var userManager = services.GetRequiredService<UserManager<AppUser>>();
-                var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
                 dbContext.Database.Migrate();
-                await SeedEntities.SeedRolesWithClaimsAsync(dbContext , roleManager);
-                await SeedEntities.SeedUserAndAdmin(dbContext, userManager);
+                await SeedEntities.SeedRolesWithClaimsAsync(dbContext);
+                await SeedEntities.SeedUserAndAdmin(dbContext);
             }
         }
     }
